ceef309846ee45c11bb26688ecd17c7d32f255b2
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-06-04 22:33:32 +0530

    Create the project folder  using spring initializer.

---
+++ Student-Management-System/src/test/java/com/rusiruchapana/SMS/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusiruchapana.SMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
