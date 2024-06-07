8fc9a45f260c7a7b760a10ed5056a75450ef4a50
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-06-07 21:54:50 +0530

    create student dto class.

---
+++ Student-Management-System/src/main/java/com/rusiruchapana/SMS/dto/StudentDto.java
@@ -0,0 +1,12 @@
+package com.rusiruchapana.SMS.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudentDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
