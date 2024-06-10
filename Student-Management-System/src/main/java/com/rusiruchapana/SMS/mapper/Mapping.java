0eb5c31dc2a80288a0c1a3e6a0eb69d6e6da4555
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-06-10 15:55:39 +0530

    create method to convert dto to entity.

--- Student-Management-System/src/main/java/com/rusiruchapana/SMS/mapper/Mapping.java
+++ Student-Management-System/src/main/java/com/rusiruchapana/SMS/mapper/Mapping.java
@@ -1,8 +1,20 @@
 package com.rusiruchapana.SMS.mapper;
 
+import com.rusiruchapana.SMS.dto.StudentDto;
 import com.rusiruchapana.SMS.entity.Student;
 
 public class Mapping {
+
+   public Student mapDtoToEntity(StudentDto studentDto){
+
+       Student student = new Student(
+               studentDto.getId(),
+               studentDto.getFirstName(),
+               studentDto.getLastName(),
+               studentDto.getEmail()
+       );
+       return student;
+   }
 
    
 
