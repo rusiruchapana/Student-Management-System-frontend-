e2fffa5ca007caf3fb645a09b4ca03a49ee526a7
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-06-11 23:05:07 +0530

    fetching data's from the controller.

--- Student-Management-System/src/main/java/com/rusiruchapana/SMS/controller/StudentController.java
+++ Student-Management-System/src/main/java/com/rusiruchapana/SMS/controller/StudentController.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/student")
 public class StudentController {
 
     @Autowired
--- frontend/src/components/StudentList.js
+++ frontend/src/components/StudentList.js
@@ -1,5 +1,26 @@
-function StudentList(){
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+function StudentList(){
+    
+    const[details , setDetails] = useState();
+
+    useEffect(() => {
+        axios.get('http://localhost:8081/api/v1/student/students')
+            .then(response => {
+                setDetails(response.data);
+            })
+            .catch(error => {
+                console.error('There was an error fetching the students!', error);
+            });
+    }, []);
+
+    return(
+        
+        <>
+            
+        </>
+    );
 }
 
 export default StudentList;
