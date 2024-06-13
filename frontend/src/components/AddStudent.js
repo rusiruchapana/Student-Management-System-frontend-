55fa9bd3db68dc8460d0f5301b97132a8eb4bca0
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-06-13 12:54:22 +0530

    modify basic skeleton of the popup model.

--- frontend/src/components/AddStudent.js
+++ frontend/src/components/AddStudent.js
@@ -21,18 +21,23 @@ function AddStudent() {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Add a Student.</Modal.Title>
         </Modal.Header>
+
+
         <Modal.Body>
           I will not close if you click outside me. Do not even try to press
           escape key.
         </Modal.Body>
+
+
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Understood</Button>
+          <Button variant="primary">Add</Button>
         </Modal.Footer>
+
       </Modal>
     </>
   );
