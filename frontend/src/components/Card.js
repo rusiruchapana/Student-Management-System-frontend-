e4e3f752f74ac38649b092480ad72f1d6f3dc0c0
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-06-13 11:00:34 +0530

    modify the card component.

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -2,9 +2,15 @@ function Card(props){
     return(
         <>
             <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-                <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="/img/erin-lindford.jpg" alt="Woman's Face" />
+                <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://avatars.githubusercontent.com/u/120650364?s=400&u=65d35b3ec16414d96ea08b2e66f70247fe61dabc&v=4" alt="Woman's Face" />
                 <div className="text-center space-y-2 sm:text-left">
                     <div className="space-y-0.5">
+                    <p className="text-lg text-black font-semibold">
+                        {props.first_name}
+                    </p>
+                    <p className="text-lg text-black font-semibold">
+                        {props.first_name}
+                    </p>
                     <p className="text-lg text-black font-semibold">
                         {props.first_name}
                     </p>
