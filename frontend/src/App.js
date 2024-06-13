e85c9c39f6e7a2b1ad07614463473fc619275765
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-06-13 12:44:12 +0530

    add a bootsrap popup model.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import StudentList from './components/StudentList';
+import AddStudent from './components/AddStudent';
 
 function App() {
   return (
     <>
       <StudentList />
+      <AddStudent />
     </>
   );
 }
