4d81d392e3be90e5bd1f4cfff1ab68db06d7c854
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-06-13 11:54:47 +0530

    passing data's to the card using props.

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -6,16 +6,16 @@ function Card(props){
                 <div className="text-center space-y-2 sm:text-left">
                     <div className="space-y-0.5">
                     <p className="text-lg text-black font-semibold">
-                        {props.first_name}
+                        {props.id}
                     </p>
                     <p className="text-lg text-black font-semibold">
                         {props.first_name}
                     </p>
                     <p className="text-lg text-black font-semibold">
-                        {props.first_name}
+                        {props.last_name}
                     </p>
                     <p className="text-slate-500 font-medium">
-                        Product Engineer
+                        {props.email}
                     </p>
                     </div>
                     <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
--- frontend/src/components/StudentList.js
+++ frontend/src/components/StudentList.js
@@ -21,7 +21,7 @@ function StudentList(){
                 //console.log(std.firstName);
                 return(
                     <div key={std.id}>
-                        <Card first_name={std.firstName}/>
+                        <Card id={std.id} first_name={std.firstName} last_name={std.lastName} email={std.email}   />
                     </div>
                 );
            })}
