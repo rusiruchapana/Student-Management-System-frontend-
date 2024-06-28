660436cf19f4763cfd55101d95603e732e91a942
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-06-28 13:15:43 +0530

    form data saved on the mysql database.

--- frontend/src/components/FormEntry.js
+++ frontend/src/components/FormEntry.js
@@ -1,4 +1,44 @@
+import { useState } from "react";
+
 function FormEntry(){
+
+    const[formData , setFormData] = useState({
+        firstName:"",
+        lastName:"",
+        email:""
+    });
+
+    const handleEvent = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          const response = await fetch('http://localhost:8081/api/v1/student/addstudent', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+    
+          if (response.ok) {
+            const result = await response.json();
+            console.log('Data saved successfully:', result);
+          } else {
+            console.error('Error saving data:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+
     return(
         <div class="flex justify-center items-center h-screen">
             <div class="w-full max-w-xs ">
@@ -7,27 +47,27 @@ function FormEntry(){
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                             FirstName
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" onChange={handleEvent} name="firstName"/>
                     </div>
 
                     <div class="mb-4">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                             LastName
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" onChange={handleEvent} name="lastName"/>
                     </div>
 
                     <div class="mb-4">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                             Email
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" onChange={handleEvent} name="email"/>
                     </div>
 
                     
 
                     <div class="flex items-center justify-between">
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleSubmit}>
                             Add
                         </button>
 
--- frontend/src/components/Student.js
+++ frontend/src/components/Student.js
@@ -5,7 +5,7 @@ function Student() {
     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
+          <th>Id</th>
           <th>First Name</th>
           <th>Last Name</th>
           <th>Username</th>
@@ -17,17 +17,6 @@ function Student() {
           <td>Mark</td>
           <td>Otto</td>
           <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
         </tr>
       </tbody>
     </Table>
