238bae8da8c4e107748329b81ed9fdf9f2b42b4a
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-06-28 19:16:39 +0530

    add a nav bar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './components/FormEntry';
 import Student from './components/Student';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+        <Navbar />
         <Student />
         
     </>
