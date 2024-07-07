41e35b20c93130f30401d4271ec571b6cfdb4812
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-07 09:28:08 +0530

    frontend.

--- Student-Management-System/src/main/java/com/rusiruchapana/SMS/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rusiruchapana.SMS.repository;
-
-import com.rusiruchapana.SMS.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student , Long> {
-}
